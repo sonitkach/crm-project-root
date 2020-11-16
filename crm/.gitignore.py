9ab859747e202c657442026484f678908b5cd50c
ASUS <sonitkach@gmail.com>
2020-11-16 11:09:10 +0300

    Add to-repr in EmployeeSerializer

---
+++ crm/.gitignore.py
@@ -0,0 +1,135 @@
+Skip
+to
+content
+Search or jump
+to…
+
+Pull
+requests
+Issues
+Marketplace
+Explore
+
+
+@sonitkach
+
+
+Learn
+Git and GitHub
+without
+any
+code!
+Using
+the
+Hello
+World
+guide, you’ll
+start
+a
+branch, write
+comments, and open
+a
+pull
+request.
+
+sonitkach
+/
+book_project
+1
+00
+Code
+Issues
+Pull
+requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+book_project /.gitignore.py /
+
+
+@sonitkach
+
+
+sonitkach
+Update.gitignore.py
+Latest
+commit
+d8d00e1
+on
+Oct
+10
+History
+1
+contributor
+50
+lines(41
+sloc)  571
+Bytes
+
+# Byte-compiled / optimized / DLL files
+__pycache__ /
+*.py[cod]
+
+# C extensions
+*.so
+
+# Distribution / packaging
+bin /
+build /
+develop - eggs /
+dist /
+eggs /
+lib /
+lib64 /
+parts /
+sdist /
+var /
+*.egg - info /
+.installed.cfg
+*.egg
+
+# Installer logs
+pip - log.txt
+pip - delete - this - directory.txt
+
+# Unit test / coverage reports
+.tox /
+.coverage
+.cache
+nosetests.xml
+coverage.xml
+
+# Translations
+*.mo
+
+# Mr Developer
+.mr.developer.cfg
+.project
+.pydevproject
+
+# Rope
+.ropeproject
+
+# Django stuff:
+*.log
+*.pot
+
+# Sphinx documentation
+docs / _build /
+© 2020
+GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact
+GitHub
+Pricing
+API
+Training
+Blog
+About
--- crm/crmapp/serializers.py
+++ crm/crmapp/serializers.py
@@ -16,9 +16,27 @@ class SpecialitySerializer(serializers.ModelSerializer):
 
 class EmployeeSerializer(serializers.ModelSerializer):
 
-    company = CompanySerializer()
-    speciality = SpecialitySerializer()
+    #company = CompanySerializer()
+    #speciality = SpecialitySerializer()
 
     class Meta:
         model = Employee
         fields = ('name', 'surname', 'company', 'speciality',)
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['company'] = CompanySerializer(instance.company).data
+        response['speciality'] = SpecialitySerializer(instance.speciality).data
+        return response
+
+    '''
+    def create(self, validated_data):
+        company_data = validated_data.pop('company')
+        company_model = Company.objects.create(**company_data)
+
+        speciality_data = validated_data.pop('speciality')
+        speciality_model = Speciality.objects.create(**speciality_data)
+
+        employee = Employee.objects.create(company=company_model, speciality = speciality_model, **validated_data)
+        return employee
+    '''
